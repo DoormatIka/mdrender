c15e247501649c557f8ac166ff9c89e2031486a3
DoormatIka <yomotouhou@gmail.com>
2026-07-19 12:10:49 +0800

    main.rs moved text function into separate function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use mdrenderlib::parser::parser;
 use std::fs;
 
 fn main() {
+    // let a = parser::BlockParser::match_heading("### text ###");
+    parse_text_file();
+}
+
+fn parse_text_file() {
     let contents = fs::read_to_string("tests/bless.txt").unwrap();
 
     let mut parsee = parser::BlockParser::new();
